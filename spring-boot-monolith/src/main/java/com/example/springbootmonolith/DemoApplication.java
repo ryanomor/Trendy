1427f75ce304cee660b92957aaa1475b924f6b43
ryanomor <omoruyi.ryan@gmail.com>
2018-06-06 23:26:34 -0400

    configure backend

--- spring-boot-monolith/src/main/java/com/example/springbootmonolith/DemoApplication.java
+++ spring-boot-monolith/src/main/java/com/example/springbootmonolith/DemoApplication.java
@@ -1,33 +1,57 @@
 package com.example.springbootmonolith;
 
+import com.netflix.ribbon.proxy.annotation.Http;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
-public class DemoApplication {
+@EnableZuulProxy
+public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 
-	@Configuration
-	public class CorsConfig {
+	@Bean
+	public CorsFilter corsFilter() {
 
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurerAdapter() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					// Grants access to any origin that requests
-					registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedOrigins("*")
-							.allowedHeaders("*");
-				}
-			};
-		}
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowedMethods(Arrays.stream(Http.HttpMethod.values()).map(Http.HttpMethod::name).collect(Collectors.toList()));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
+
+//	@Configuration
+//	public class CorsConfig {
+//
+//		@Bean
+//		public WebMvcConfigurer corsConfigurer() {
+//			return new WebMvcConfigurerAdapter() {
+//				@Override
+//				public void addCorsMappings(CorsRegistry registry) {
+//					// Grants access to any origin that requests
+//					registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedOrigins("*")
+//							.allowedHeaders("*");
+//				}
+//			};
+//		}
+//	}
 }
